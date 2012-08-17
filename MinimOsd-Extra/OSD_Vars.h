4fd93db080deaf612e3a7e853e353793fd00c09d
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-08-17 09:27:02 +0000

    Now The stable version really is the stable version, and the pre release is..... :)

--- MinimOsd-Extra/OSD_Vars.h
+++ MinimOsd-Extra/OSD_Vars.h
@@ -10,44 +10,44 @@
 //static float aspd_error = 0; // Current airspeed error in meters/second
 //static float	xtrack_error = 0; // Current crosstrack error on x-y plane in meters
 
-static int16_t      chan1_raw = 0;
-static int16_t      chan2_raw = 0;
-static int16_t      chan1_raw_middle = 0;
-static int16_t      chan2_raw_middle = 0;
-static int          osd_set = 0;
+static int16_t chan1_raw = 0;
+static int16_t chan2_raw = 0;
+static int16_t chan1_raw_middle = 0;
+static int16_t chan2_raw_middle = 0;
+static int osd_set = 0;
 
-static int          setup_menu = 0;
-static int          converts = 0;
-static int          converth = 0;
-static uint8_t     overspeed = 0;
-static uint8_t     stall = 0;
-static uint8_t     battv = 0;                //Batery warning voltage - units Volt *10 
-//static int        battp = 0;
+static int setup_menu = 0;
+static int converts = 0;
+static int converth = 0;
+static int overspeed = 0;
+static int stall = 0;
+static float battv = 0;
+//static int battp = 0;
 
-static uint8_t      spe = 0;
-static uint8_t      high = 0;
-static float        osd_vbat_A = 0;                 // Battery A voltage in milivolt
-static float        osd_curr_A = 0;                 // Battery A current
-static uint16_t     osd_battery_remaining_A = 0;    // 0 to 100 <=> 0 to 1000
-static uint8_t      osd_battery_pic_A = 0xb4;       // picture to show battery remaining
-//static float      osd_vbat_B = 0;               // voltage in milivolt
-//static float      osd_curr_B = 0;                 // Battery B current
-//static uint16_t   osd_battery_remaining_B = 0;  // 0 to 100 <=> 0 to 1000
-//static uint8_t    osd_battery_pic_B = 0xb4;     // picture to show battery remaining
-static int          start_Time = -1;
-static uint16_t     osd_mode = 0;                   // Navigation mode from RC AC2 = CH5, APM = CH8
-static uint8_t      osd_nav_mode = 0;               // Navigation mode from RC AC2 = CH5, APM = CH8
-static int          text_blink = 0;
-static uint8_t      warning_type = 0;
-//static int        char_update = 0;
-static int          osd_off = 0;
-static uint16_t     osd_off_switch = 0;
-static uint16_t     osd_switch_last = 100;
-static long         osd_switch_time = 0;
-static long         wind_time = 0;
-static float        osd_climb = 0;
-static long         a = 0;
-static long         b = 0;
+static uint8_t spe = 0;
+static uint8_t high = 0;
+static float    osd_vbat_A = 0;                 // Battery A voltage in milivolt
+static float    osd_curr_A = 0;                 // Battery A current
+static uint16_t osd_battery_remaining_A = 0;    // 0 to 100 <=> 0 to 1000
+static uint8_t  osd_battery_pic_A = 0xb4;       // picture to show battery remaining
+//static float    osd_vbat_B = 0;               // voltage in milivolt
+//static float    osd_curr_B = 0;                 // Battery B current
+//static uint16_t osd_battery_remaining_B = 0;  // 0 to 100 <=> 0 to 1000
+//static uint8_t  osd_battery_pic_B = 0xb4;     // picture to show battery remaining
+static int start_Time = -1;
+static uint16_t osd_mode = 0;                   // Navigation mode from RC AC2 = CH5, APM = CH8
+static uint8_t  osd_nav_mode = 0;               // Navigation mode from RC AC2 = CH5, APM = CH8
+static int text_blink = 0;
+static int warning_type = 0;
+static int char_update = 0;
+static int osd_off = 0;
+static uint16_t osd_off_switch = 0;
+static uint16_t osd_switch_last = 100;
+static long osd_switch_time = 0;
+static long wind_time = 0;
+static float osd_climb = 0;
+static long a = 0;
+static long b = 0;
 
 static float    osd_lat = 0;                    // latidude
 static float    osd_lon = 0;                    // longitude
@@ -61,25 +61,27 @@ static float    osd_home_alt = 0;
 static long     osd_home_distance = 0;          // distance from home
 static uint8_t  osd_home_direction;             // Arrow direction pointing to home (1-16 to CW loop)
 
-static int8_t       osd_pitch = 0;                  // pitch form DCM
-static int8_t       osd_roll = 0;                   // roll form DCM
-static int8_t       osd_yaw = 0;                    // relative heading form DCM
-static float        osd_heading = 0;                // ground course heading from GPS
-static float        heding_check = -2;
-static float        osd_alt = 0;                    // altitude
-static float        osd_airspeed = -1;              // air speed
-static float        osd_windspeed = 0;
-static float        osd_windspeed_check = 0;
-static float        osd_winddirection = 0;
-static float        osd_winddirection_check = 0;
-static float        osd_wind_arrow_rotate = 0;
-static uint8_t      osd_wind_arrow_rotate_int;
+static int8_t      osd_pitch = 0;                  // pitch form DCM
+static int8_t      osd_roll = 0;                   // roll form DCM
+static int8_t      osd_yaw = 0;                    // relative heading form DCM
+static float    osd_heading = 0; // ground course heading from GPS
+static float   heding_check = -2;
+static float    osd_alt = 0;                    // altitude
+static float    osd_airspeed = -1;               // air speed
+static float    osd_windspeed = 0;
+static float osd_windspeed_check = 0;
+static float osd_winddirection = 0;
+static float osd_winddirection_check = 0;
+static float osd_wind_arrow_rotate = 0;
 
 static int wind = 0;
-//static int osd_wind_arrow = 0;
-
+static int osd_wind_arrow = 0;
+static char wind_arrow_set1 = 0x20;
+static char wind_arrow_set2 = 0x20;
 static uint8_t  osd_alt_cnt = 0;              // counter for stable osd_alt
 static float    osd_alt_prev = 0;             // previous altitude
+
+
 
 static float    osd_groundspeed = 0;            // ground speed
 static uint16_t osd_throttle = 0;               // throtle
@@ -105,7 +107,7 @@ byte SerCMD1 = 0;
 byte SerCMD2 = 0;
 
 
-//int tempvar;      // Temporary variable used on many places around the OSD
+int tempvar;      // Temporary variable used on many places around the OSD
 
 // First 8 panels and their X,Y coordinate holders
 byte panCenter_XY[2]; // = { 13,7,0 };
