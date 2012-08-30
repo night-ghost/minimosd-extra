45cc0e776ebdf028e4be6be6c828346355a04472
pdrsantos.santos@gmail.com <pdrsantos.santos@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-08-30 14:20:42 +0000

    MinimOSD-Extra Beta: Modified code to be in line with the pre-release.

--- trunk/Beta version/OSD_Vars.h
+++ trunk/Beta version/OSD_Vars.h
@@ -14,40 +14,43 @@ static int16_t      chan1_raw = 0;
 static int16_t      chan2_raw = 0;
 static int16_t      chan1_raw_middle = 0;
 static int16_t      chan2_raw_middle = 0;
+static int16_t      ch_off = 0;
 static int          osd_set = 0;
 
-static int          setup_menu = 0;
-static int          converts = 0;
-static int          converth = 0;
+static int8_t       setup_menu = 0;
+static float        converts = 0;
+static float        converth = 0;
 static uint8_t      overspeed = 0;
 static uint8_t      stall = 0;
-static uint8_t      battv = 0;                //Batery warning voltage - units Volt *10 
+static uint8_t      battv = 0;                //Battery warning voltage - units Volt *10 
 //static int        battp = 0;
 
 static uint8_t      spe = 0;
 static uint8_t      high = 0;
 static float        osd_vbat_A = 0;                 // Battery A voltage in milivolt
-static float        osd_curr_A = 0;                 // Battery A current
-static uint16_t     osd_battery_remaining_A = 0;    // 0 to 100 <=> 0 to 1000
-//static uint8_t      osd_battery_pic_A = 0xb4;       // picture to show battery remaining
+static int16_t      osd_curr_A = 0;                 // Battery A current
+static int8_t       osd_battery_remaining_A = 0;    // 0 to 100 <=> 0 to 1000
+static uint8_t      osd_battery_pic_A = 0xb4;       // picture to show battery remaining
 //static float      osd_vbat_B = 0;               // voltage in milivolt
 //static float      osd_curr_B = 0;                 // Battery B current
 //static uint16_t   osd_battery_remaining_B = 0;  // 0 to 100 <=> 0 to 1000
 //static uint8_t    osd_battery_pic_B = 0xb4;     // picture to show battery remaining
-static int          start_Time = -1;
+static float        start_Time = -1.0;
 static uint16_t     osd_mode = 0;                   // Navigation mode from RC AC2 = CH5, APM = CH8
 static uint8_t      osd_nav_mode = 0;               // Navigation mode from RC AC2 = CH5, APM = CH8
-static int          text_blink = 0;
+static unsigned long        text_timer = 0;
+
 static uint8_t      warning_type = 0;
+static uint8_t      last_warning = 0;
 //static int        char_update = 0;
-static int          osd_off = 0;
+static int          osd_on = 1;
 static uint16_t     osd_off_switch = 0;
 static uint16_t     osd_switch_last = 100;
-static long         osd_switch_time = 0;
-static long         wind_time = 0;
+static unsigned long         osd_switch_time = 0;
+static unsigned long         wind_time = 0;
 static float        osd_climb = 0;
-static long         a = 0;
-static long         b = 0;
+
+
 
 static float        osd_lat = 0;                    // latidude
 static float        osd_lon = 0;                    // longitude
@@ -61,20 +64,20 @@ static float        osd_home_alt = 0;
 static long         osd_home_distance = 0;          // distance from home
 static uint8_t      osd_home_direction;             // Arrow direction pointing to home (1-16 to CW loop)
 
-static int8_t       osd_pitch = 0;                  // pitch form DCM
-static int8_t       osd_roll = 0;                   // roll form DCM
+static int8_t       osd_pitch = 0;                  // pitch from DCM
+static int8_t       osd_roll = 0;                   // roll from DCM
 static int8_t       osd_yaw = 0;                    // relative heading form DCM
 static float        osd_heading = 0;                // ground course heading from GPS
-static float        heding_check = -2;
+
 static float        osd_alt = 0;                    // altitude
-static float        osd_airspeed = -1;              // air speed
+static float        osd_airspeed = -1;              // airspeed
 static float        osd_windspeed = 0;
 static float        osd_windspeedz = 0;
-static float        osd_windspeed_check = 0;
+
 static float        osd_winddirection = 0;
-static float        osd_winddirection_check = 0;
-static float        osd_wind_arrow_rotate = 0;
-static uint8_t      osd_wind_arrow_rotate_int;
+
+
+static int8_t       osd_wind_arrow_rotate_int;
 
 static int          wind = 0;
 //static int osd_wind_arrow = 0;
@@ -85,6 +88,8 @@ static float        osd_alt_prev = 0;             // previous altitude
 static float        osd_groundspeed = 0;            // ground speed
 static uint16_t     osd_throttle = 0;               // throtle
 
+static float  ch_raw = 0;
+
 //MAVLink session control
 static boolean      mavbeat = 0;
 static float        lastMAVBeat = 0;
@@ -94,12 +99,8 @@ static uint8_t      apm_mav_system;
 static uint8_t      apm_mav_component;
 static boolean      enable_mav_request = 0;
 
-static uint8_t      delay_setup = 0;
-
-
-static const uint8_t npanels = 2; 
+static const uint8_t npanels = 2;
 static uint8_t panel = 0; 
-
 // Panel BIT registers
 byte panA_REG[npanels] = {0b00000000};
 byte panB_REG[npanels] = {0b00000000};
@@ -132,7 +133,7 @@ byte panHomeDis_XY[2][npanels]; // = { 0,0 };
 byte panWPDir_XY[2][npanels]; // = { 0,0 };
 byte panWPDis_XY[2][npanels]; // = { 0,0 };
 byte panTime_XY[2][npanels];
-//byte panRSSI_XY[2] = { 0,0 };
+
 
 // Third set of panels and their X,Y coordinate holders
 byte panCur_A_XY[2][npanels]; // = { 23,1 };
@@ -148,10 +149,19 @@ byte panHorizon_XY[2][npanels]; // = {8,centercalc}
 // Third set of panels and their X,Y coordinate holders
 byte panWarn_XY[2];
 byte panOff_XY[2];
-byte panWindSpeed_XY[2];
-byte panClimb_XY[2];
+byte panWindSpeed_XY[2][npanels];
+byte panClimb_XY[2][npanels];
 //byte panTune_XY[2];
 byte panSetup_XY[2];
+byte panRSSI_XY[2][npanels];
 
-
-
+//*************************************************************************************************************
+//rssi varables
+static uint8_t  rssi = 0;
+static uint8_t  rssipersent = 0;
+static uint8_t  rssical = 0;
+static uint8_t  osd_rssi = 0;
+static uint8_t  radio_setup_flag = 0;
+static int16_t  osd_chan6_raw = 1000;
+static int16_t  osd_chan7_raw = 1000;
+static int16_t  osd_chan8_raw = 1000;
