608f19b19e43e9bb2af4858b87ddb51ddf3e749e
pdrsantos.santos@gmail.com <pdrsantos.santos@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-09-17 21:18:33 +0000

    MinimOSD-Extra Pre-release: changed momentary switching number.

--- MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -206,7 +206,7 @@ void panWindSpeed(int first_col, int first_line){
 // Staus  : done
 
 void panOff(){
-    if (ch_toggle == 0){
+    if (ch_toggle == 4){
         if (((apm_mav_type == 1) && ((osd_mode != 11) && (osd_mode != 1))) || ((apm_mav_type == 2) && ((osd_mode != 6) && (osd_mode != 7)))){
             if (osd_off_switch != osd_mode){ 
                 osd_off_switch = osd_mode;
