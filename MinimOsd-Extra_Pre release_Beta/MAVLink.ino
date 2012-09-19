22cc0ee9e46a1890447df3933821f88208687217
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-09-19 15:07:43 +0000

    Added DISARMED warning

--- MinimOsd-Extra_Pre release_Beta/MAVLink.ino
+++ MinimOsd-Extra_Pre release_Beta/MAVLink.ino
@@ -61,7 +61,7 @@ void read_mavlink(){
                     osd_mode = (uint8_t)mavlink_msg_heartbeat_get_custom_mode(&msg);
                     //Mode (arducoper armed/disarmed)
                     base_mode = mavlink_msg_heartbeat_get_base_mode(&msg);
-                    if(getBit(base_mode,7) == true) motor_armed = 1;
+                    if(getBit(base_mode,7) == 0b10000000) motor_armed = 1;
                     else motor_armed = 0;
 
                     osd_nav_mode = 0;          
