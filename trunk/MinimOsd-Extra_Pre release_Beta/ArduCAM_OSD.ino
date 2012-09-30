356f116451e68c929d9a44aee4a8c49ed7939f42
pdrsantos.santos@gmail.com <pdrsantos.santos@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-09-30 17:25:10 +0000

    MinimOSD-Extra Pre-release: added brightness control.

--- trunk/MinimOsd-Extra_Pre release_Beta/ArduCAM_OSD.ino
+++ trunk/MinimOsd-Extra_Pre release_Beta/ArduCAM_OSD.ino
@@ -45,6 +45,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>
 #undef PSTR 
 #define PSTR(s) (__extension__({static prog_char __c[] PROGMEM = (s); &__c[0];})) 
 
+#define isPAL 1
 
 /* **********************************************/
 /* ***************** INCLUDES *******************/
