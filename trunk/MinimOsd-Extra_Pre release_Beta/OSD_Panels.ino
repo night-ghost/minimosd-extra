6f8fb47a2f9b49c9921d037d3bdae20e3dda5d2e
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2012-10-23 10:33:09 +0000

    Changed timer panel to watch bough, throttle and hight to activate.

--- trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -11,7 +11,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\x20\x20\x20\x20\x20\xba\xbb\xbc\xbd\xbe|\x20\x20\x20\x20\x20\xca\xcb\xcc\xcd\xce|MinimOSD Extra|Pre-release 2.1.5 r428"));
+    osd.printf_P(PSTR("\x20\x20\x20\x20\x20\xba\xbb\xbc\xbd\xbe|\x20\x20\x20\x20\x20\xca\xcb\xcc\xcd\xce|MinimOSD Extra|Pre-release 2.1.5 r429"));
     osd.closePanel();
 }
 
@@ -602,7 +602,7 @@ void panTime(int first_col, int first_line){
     osd.setPanel(first_col, first_line);
     osd.openPanel();
     
-    if (takeofftime == 0 && osd_throttle > 5) 
+    if (haltset == 1 && takeofftime == 0 && (osd_alt - osd_home_alt) > 5 && osd_throttle > 5) 
     {
     takeofftime = 1;
     FTime = (millis()/1000);
