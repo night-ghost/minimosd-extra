b8f615c79970e9e64107143800eff08bba47cf04
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-02-25 09:39:51 +0000

    Filled in the base code for Copter and Miguels alternate FW

---
+++ trunk/MinimOsd-Alternate by Miguel Pereira/Spi.h
@@ -0,0 +1,27 @@
+#ifndef Spi_h
+#define Spi_h
+
+// Get the common arduino functions
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "Arduino.h"
+#else
+	#include "wiring.h"
+#endif
+
+#define SCK_PIN   13
+#define MISO_PIN  12
+#define MOSI_PIN  11
+#define SS_PIN    10  // <------- !!! (Remember! This pin will select USB host chip Max3421)
+
+class SPI
+{
+  public:
+    SPI(void);
+    void mode(byte);
+    byte transfer(byte);
+    byte transfer(byte, byte);
+};
+
+extern SPI Spi;
+
+#endif
