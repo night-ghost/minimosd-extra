023416cbf5a3ed4b2d79eb8845ce45e4b4ebe546
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-03-19 21:43:33 +0000

    A small fix for crosstrack Also made a mcm for dotted horizon and Jörgs smooth horizon

--- MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xba\xbb\xbc\xbd\xbe|\xca\xcb\xcc\xcd\xce|MinimOSD Extra 2.2"));
+    osd.printf_P(PSTR("\xba\xbb\xbc\xbd\xbe|\xca\xcb\xcc\xcd\xce|MinimOSD-Extra|Pre-Release r499"));
     osd.closePanel();
 }
 
@@ -968,8 +968,8 @@ void panWPDis(int first_col, int first_line){
     wp_target_bearing_rotate_int = round(((float)wp_target_bearing - osd_heading)/360.0 * 16.0) + 1; //Convert to int 0-16 
     if(wp_target_bearing_rotate_int < 0 ) wp_target_bearing_rotate_int += 16; //normalize
     
-//      if (xtrack_error > 999) xtrack_error = 999;
-//      if (xtrack_error < -999) xtrack_error = -999;
+      if (xtrack_error > 999) xtrack_error = 999;
+      if (xtrack_error < -999) xtrack_error = -999;
 
       osd.printf("%c%c%2i%c%4.0f%c|",0x57, 0x70, wp_number,0x0,(double)((float)(wp_dist) * converth),high);
       showArrow((uint8_t)wp_target_bearing_rotate_int,0);
