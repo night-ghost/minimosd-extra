faed19ca28114699b6eb62da867e5c043d004154
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-03-25 21:27:35 +0000

    Made Battery percent or mah used selectable in FW

--- trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Pre-Release r519"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Pre-Release r521"));
     osd.closePanel();
 }
 
@@ -695,8 +695,11 @@ void panBatteryPercent(int first_col, int first_line){
     osd.setPanel(first_col, first_line);
     osd.openPanel();
     
-    osd.printf("%c%3.0i%c|%4.0f%c", 0x17, osd_battery_remaining_A, 0x25, mah_used, 0xbe);
-    
+    if (EEPROM.read(panMavBeat_en_ADDR) == 0){
+    osd.printf("%c%3.0i%c", 0x17, osd_battery_remaining_A, 0x25);
+    }else{
+    osd.printf("%c%4.0f%c",0x17, mah_used, 0xbe);
+    }
     osd.closePanel();
 }
 
