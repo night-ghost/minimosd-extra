80bed01e963a79a9e320a6168f19fce979663be3
vitor.rosado@gmail.com <vitor.rosado@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-04-03 11:45:23 +0000

    Plane: Bug fix in ILS Saving more bytes changing from "switch/case" to "if/else if"

--- trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Pre-Release r548"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Pre-Release r549"));
     osd.closePanel();
 }
 
