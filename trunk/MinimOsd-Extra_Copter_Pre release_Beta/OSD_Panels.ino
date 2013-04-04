e1ecfd6ecfd33cc6054e92cad4a3ca3d02082bbd
vitor.rosado@gmail.com <vitor.rosado@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-04-04 19:51:25 +0000

    Copter: Rose fix

--- trunk/MinimOsd-Extra_Copter_Pre release_Beta/OSD_Panels.ino
+++ trunk/MinimOsd-Extra_Copter_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra Copter|Pre-Release r554"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra Copter|Pre-Release r557"));
     osd.closePanel();
 }
 
