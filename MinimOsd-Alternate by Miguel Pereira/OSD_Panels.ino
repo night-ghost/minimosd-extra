8d369f5f3b9601778e17b45c3ebce4c9ed1536c2
vitor.rosado@gmail.com <vitor.rosado@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-05-27 14:52:11 +0000

    Copter: Home arrow bug fix Real heading arrow bug fix Time of flight bug fix when timer panel not selected

--- MinimOsd-Alternate by Miguel Pereira/OSD_Panels.ino
+++ MinimOsd-Alternate by Miguel Pereira/OSD_Panels.ino
@@ -10,7 +10,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra Copter|Pre-Release r598"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra Copter|Pre-Release r599"));
     osd.closePanel();
 }
 
@@ -126,17 +126,16 @@ void panCOG(int first_col, int first_line){
     osd.setPanel(first_col, first_line);
     osd.openPanel();
     
-    osd_COG_arrow_rotate_int = round(((osd_cog / 100) - osd_heading) / 360.0 * 15.0 + 1); //Convert to int 1-16 
-    if(osd_COG_arrow_rotate_int < 1 ) osd_COG_arrow_rotate_int += 16;
-    //if(osd_COG_arrow_rotate_int == 0) osd_COG_arrow_rotate_int = 16;    
-    //if(osd_COG_arrow_rotate_int == 17) osd_COG_arrow_rotate_int = 1;
-    
-    off_course = (osd_cog / 100 - osd_heading);
+    osd_COG_arrow_rotate_int = (osd_cog / 100 - osd_heading); //[-360, 360]
+    if(osd_COG_arrow_rotate_int < 0 ) osd_COG_arrow_rotate_int += 360; //Normalize [0, 360]
+    off_course = osd_COG_arrow_rotate_int;
+    //off_course = (osd_cog / 100 - osd_heading);
     if (off_course > 180){
        off_course += - 360;
     }else if (off_course < -180){
        off_course += + 360;
     }
+    osd_COG_arrow_rotate_int = (int)round((float)(osd_COG_arrow_rotate_int/360.0) * 16.0) % 16 + 1; //[1, 16]
     
     showArrow((uint8_t)osd_COG_arrow_rotate_int,2);
 
@@ -686,7 +685,7 @@ void panTime(int first_col, int first_line){
     osd.setPanel(first_col, first_line);
     osd.openPanel();
     
-    start_Time = (millis()/1000) - FTime;
+    //start_Time = (millis()/1000) - FTime;
     osd.printf("%c%2i%c%02i", 0x08,((int)start_Time/60)%60,0x3A,(int)start_Time%60);
     osd.closePanel();
 }
