b0c93d4e370558b5131eb230da6f06907346ce55
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-06-13 17:01:02 +0000

    Tuned VS. It is a bit more sensitive now. (Less latency)

--- MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Beta r607"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Beta r608"));
     osd.closePanel();
 }
 
@@ -570,7 +570,7 @@ void panAlt(int first_col, int first_line){
 void panClimb(int first_col, int first_line){
     osd.setPanel(first_col, first_line);
     osd.openPanel();
-    vs = (osd_climb * converth * 60) * 0.05 + vs * 0.95;
+    vs = (osd_climb * converth * 60) * 0.1 + vs * 0.9;
     osd.printf("%c%4.0f%c",0x15, int(vs / 10.0) * 10.0, climbchar);
     osd.closePanel();
 }
