62d34fed7969d495cb7399346a896f65c414efca
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-06-20 18:25:33 +0000

    Temporary fix for wind

--- trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
+++ trunk/MinimOsd-Extra_Pre release_Beta/OSD_Panels.ino
@@ -12,7 +12,7 @@ void startPanels(){
 void panLogo(){
     osd.setPanel(5, 5);
     osd.openPanel();
-    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Beta r610"));
+    osd.printf_P(PSTR("\xb0\xb1\xb2\xb3\xb4|\xb5\xb6\xb7\xb8\xb9|MinimOSD-Extra|Beta r611"));
     osd.closePanel();
 }
 
@@ -412,10 +412,13 @@ void panWindSpeed(int first_col, int first_line){
 //    osd_wind_arrow_rotate_int += 8;
 //    }
 //    nor_osd_windspeed = osd_windspeed * 0.005 + nor_osd_windspeed * 0.995;
-
+    if (osd_winddirection < 0){
+    osd_wind_arrow_rotate_int = round(((osd_winddirection + 360) - osd_heading)/360.0 * 16.0) + 9; //Convert to int 1-16
+    }else{
     osd_wind_arrow_rotate_int = round((osd_winddirection - osd_heading)/360.0 * 16.0) + 9; //Convert to int 1-16
+    }
     if(osd_wind_arrow_rotate_int > 16 ) osd_wind_arrow_rotate_int -= 16; //normalize
-    if(osd_wind_arrow_rotate_int < 0 ) osd_wind_arrow_rotate_int += 16; //normalize
+    if(osd_wind_arrow_rotate_int < 1 ) osd_wind_arrow_rotate_int += 16; //normalize
     nor_osd_windspeed = osd_windspeed * 0.010 + nor_osd_windspeed * 0.990;    
 
     showArrow((uint8_t)osd_wind_arrow_rotate_int,1); //print data to OSD
