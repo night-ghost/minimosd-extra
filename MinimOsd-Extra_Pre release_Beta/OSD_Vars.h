c5493d75a0a263e900e008cee25953daa0d3d6d0
gabeki17@gmail.com <gabeki17@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2013-07-17 11:54:06 +0000

    Added a check to one_sec_timer if it is true, before all the panels run.

--- MinimOsd-Extra_Pre release_Beta/OSD_Vars.h
+++ MinimOsd-Extra_Pre release_Beta/OSD_Vars.h
@@ -148,6 +148,7 @@ static uint8_t      apm_mav_system;
 static uint8_t      apm_mav_component;
 static boolean      enable_mav_request = 0;
 static boolean      blinker = 0;
+static boolean      one_sec_timer_switch = 0;
 
 static const uint8_t npanels = 2;
 static uint8_t panel = 0; 
