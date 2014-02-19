8a2cb5cda6ac76956693b669c2b42b9792921ab2
vitor.rosado@gmail.com <vitor.rosado@gmail.com@4a4ecd91-15e1-0376-24c1-b6f0f5c9b0df>
2014-02-19 21:25:10 +0000

    Font improvement saving 102 bytes ( tested :) ) Needs new CT Tool method which is already in "dev functions".

--- MinimOsd-Extra_Copter_Pre release_Beta/Font.ino
+++ MinimOsd-Extra_Copter_Pre release_Beta/Font.ino
@@ -3,21 +3,14 @@ void uploadFont()
 {
     uint16_t byte_count = 0;
     byte bit_count;
-    byte ascii_binary[0x08];
 
     // move these local to prevent ram usage
     uint8_t character_bitmap[0x40];
     int font_count = 0;
 
     osd.clear();
-//    osd.setPanel(6,9);
-//    osd.openPanel();
-//    osd.printf_P(PSTR("Character Update"));
-//    delay(2000);
-//    osd.closePanel();
 
-
-    Serial.printf_P("Ready for Font\n");
+    Serial.printf_P(PSTR("RFF\n"));
 
     while(font_count < 256) { 
         int8_t incomingByte = Serial.read();
@@ -25,52 +18,18 @@ void uploadFont()
         {
         case 0x0d: // carridge return, end of line
             //Serial.println("cr");
-            if (bit_count == 8 && (ascii_binary[0] == 0x30 || ascii_binary[0] == 0x31))
+            if (bit_count == 8)
             {
-                // turn 8 ascii binary bytes to single byte '01010101' = 0x55
-                // fill in 64 bytes of character data
-                // made this local to prevent needing a global
-                byte ascii_byte;
-
-                ascii_byte = 0;
-
-                if (ascii_binary[0] == 0x31) // ascii '1'
-                    ascii_byte = ascii_byte + 128;
-
-                if (ascii_binary[1] == 0x31)
-                    ascii_byte = ascii_byte + 64;
-
-                if (ascii_binary[2] == 0x31)
-                    ascii_byte = ascii_byte + 32;
-
-                if (ascii_binary[3] == 0x31)
-                    ascii_byte = ascii_byte + 16;
-
-                if (ascii_binary[4] == 0x31)
-                    ascii_byte = ascii_byte + 8;
-
-                if (ascii_binary[5] == 0x31)
-                    ascii_byte = ascii_byte + 4;
-
-                if (ascii_binary[6] == 0x31)
-                    ascii_byte = ascii_byte + 2;
-
-                if (ascii_binary[7] == 0x31)
-                    ascii_byte = ascii_byte + 1;
-
-                character_bitmap[byte_count] = ascii_byte;
                 byte_count++;
-                bit_count = 0;
+                character_bitmap[byte_count] = 0;
             }
-            else
-                bit_count = 0;
-            break;
-        case 0x0a: // line feed, ignore
-            //Serial.println("ln");   
+            bit_count = 0;
             break;
         case 0x30: // ascii '0'
         case 0x31: // ascii '1' 
-            ascii_binary[bit_count] = incomingByte;
+            character_bitmap[byte_count] = character_bitmap[byte_count] << 1;
+            if(incomingByte == 0x31)
+              character_bitmap[byte_count] += 1;
             bit_count++;
             break;
         default:
@@ -84,12 +43,9 @@ void uploadFont()
             osd.write_NVM(font_count, character_bitmap);    
             byte_count = 0;
             font_count++;
-            Serial.printf_P("Char Done\n");
+            Serial.printf_P(PSTR("CD\n"));
         }
     }
-
-    //  character_bitmap[]
 }
-
 
 
